c1f1b560bd466f1f802fc1bd8c38d356608606b3
boodovskiy <dzak1987@mail.ru>
2025-04-01 10:09:43 +0300

    Add Room Card component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import rooms from '../data/rooms.json'
+import RoomCard from './components/RoomCard';
 
 export default function Home() {
   return (
     <>
       { rooms.length > 0 ? (
-        rooms.map((room, index) => <h3 key={index}>{ room.name }</h3>)
+        rooms.map((room, index) => <RoomCard key={index} room={room} />)
         ) : (<p>No rooms available at the moment.</p>)
       }
     </>
