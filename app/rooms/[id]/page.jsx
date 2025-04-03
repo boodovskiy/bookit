234f8a1c1d8d6dadfdd48281c3695a0288cdaf18
boodovskiy <dzak1987@mail.ru>
2025-04-03 10:06:13 +0300

    Add Room details page component

---
+++ app/rooms/[id]/page.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import rooms from '@/data/rooms.json'
+import Heading from '@/app/components/Heading';
+import { FaChevronLeft } from 'react-icons/fa';
+import Image from 'next/image';
+
+const RoomPage = async ({ params }) => {
+    const { id } = await params;
+    const room = rooms.find((room) => room.$id === id);
+
+    if (!room) {
+        return <Heading title='Room Not Found!' />
+    }
+  return (
+    <>
+        <Heading title={ room.name } />
+        <div className="bg-white shadow rounded-lg p-6">
+            <a href="/rooms.html" className='flex items-center text-gray-600 hover:text-gray-800 mb-4'>
+                <FaChevronLeft className='inline mr-1'/>
+                <span className="ml-2">Back to Rooms</span>
+            </a>
+
+            <div className="flex flex-col sm:flex-row sm:space-x-6">
+                <Image 
+                    src={`/images/rooms/${room.image}`}
+                    width={400}
+                    height={100}
+                    alt={room.name} 
+                    className='w-full sm:w-1/3 h-64 object-cover rounded-lg' 
+                />
+                <div className="mt-4 sm:mt-0 sm:flex-1">
+                    <p className="text-gray-600 mb-4">
+                        { room.description }
+                    </p>
+
+                    <ul className='space-y-2'>
+                        <li>
+                            <span className="font-semibold text-gray-800">Size: </span> {room.sqft}
+                        </li>
+                        <li>
+                            <span className="font-semibold text-gray-800">Availability: </span>
+                            {room.availability}
+                        </li>
+                        <li>
+                            <span className="font-semibold text-gray-800">Price: </span>
+                            ${room.price_per_hour}/hour
+                        </li>
+                        <li>
+                            <span className="font-semibold text-gray-800">Address: </span> {room.address}
+                        </li>
+                    </ul> 
+                </div>
+            </div>
+         
+        </div>
+    </>
+  )
+}
+
+export default RoomPage
