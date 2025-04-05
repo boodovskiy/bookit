b4d8d6b80cfa6318ba1dbcef9ca81d7aea46abe9
boodovskiy <dzak1987@mail.ru>
2025-04-05 12:11:53 +0300

    Minor fixes

--- app/page.js
+++ app/page.js
@@ -4,12 +4,12 @@ import RoomCard from './components/RoomCard';
 
 export default async function Home() {
   const rooms = await getAllRooms();
-  
+
   return (
     <>
       <Heading title='Available Rooms' />
       { rooms.length > 0 ? (
-        rooms.map((room, index) => <RoomCard key={index} room={room} />)
+        rooms.map((room, index) => <RoomCard key={room.$id} room={room} />)
         ) : (<p>No rooms available at the moment.</p>)
       }
     </>
