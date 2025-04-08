584bb4a4fd42705a43fc729f836bbd7adb6eb60b
boodovskiy <dzak1987@mail.ru>
2025-04-08 09:55:25 +0300

    Protect bookins route, add createSession action and add form action for Login Page

---
+++ app/bookings/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookingsPage = () => {
+  return (
+    <>BookingsPage</>
+  )
+}
+
+export default BookingsPage
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,23 +1,32 @@
+'use client';
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
+import createSession from '../actions/createSession'
+import { useFormState } from 'react-dom';
 
 const LoginPage = () => {
+    const [state, formAction] = useFormState(createSession, {});
+
+    useEffect(() => {
+        if (state.error) console.log(state.error);
+    }, [state])
+
   return (
     <div className='flex items-center justify-center'>
         <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm mt-20">
-            <form>
+            <form action={formAction}>
                 <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
                     Login
                 </h2>
 
-                <div className="mb-4">
-                    <label htmlFor="password" className='block text-gray-700 font-bold mb-2'>Email</label>
-                    <input type="password" id="password" name='password' className="border rounded w-full py-2 px-3" required />
-                </div>
-
                 <div className="mb-6">
                     <label htmlFor="email" className='block text-gray-700 font-bold mb-2'>Email</label>
                     <input type="email" id="email" name='email' className="border rounded w-full py-2 px-3" required />
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="password" className='block text-gray-700 font-bold mb-2'>Password</label>
+                    <input type="password" id="password" name='password' className="border rounded w-full py-2 px-3" required />
                 </div>
 
                 <div className="flex flex-col gap-5">
