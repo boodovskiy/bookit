6fb73f5510d4fd498d162686ca40b76d2c51fe6f
boodovskiy <dzak1987@mail.ru>
2025-04-19 11:50:41 +0300

    Add MyRoomCard compontent for listing on MyRooms page

--- app/rooms/my/page.jsx
+++ app/rooms/my/page.jsx
@@ -1,5 +1,6 @@
 import getMyRooms from '@/app/actions/getMyRooms';
 import Heading from '@/app/components/Heading';
+import MyRoomCard from '@/app/components/MyRoomCard';
 import React from 'react'
 
 const MyRoomsPage = async () => {
@@ -8,7 +9,7 @@ const MyRoomsPage = async () => {
     <>
         <Heading title='My Rooms' />
         {rooms.length > 0 ? (
-            rooms.map((room) => <h3>{ room.name }</h3>  ) 
+            rooms.map((room) => <MyRoomCard key={room.$id} room={room}/>  ) 
         ) : (
             <div className="bg-white shadow rounded-lg p-6">
                 <h3 className='text-gray-600'>You have not created any rooms yet.</h3>
